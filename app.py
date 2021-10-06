71cb1e11ae8a1535edf55b6130a152abb812f1f8
maciek <maciej.morzywolek@gmail.com>
2021-10-06 22:47:59 +0200

    new class attributes

--- app.py
+++ app.py
@@ -1,34 +1,49 @@
 import streamlit as st
 import pandas as pd
 from data_pull import Logowanie
+
 # ---------------------------------#
 # Page layout
 ## Page expands to full width
 st.set_page_config(page_title='Facebook Insight API data connector',
                    layout='wide')
 # ---------------------------------#
-st.sidebar.title("1. Enter your Facebook Developer API credentials:")
-app_id = st.sidebar.text_input(label="App ID")
-app_secret = st.sidebar.text_input(label="App Secret", type="password")
-access_token = st.sidebar.text_input(label="Access Token")
-ad_account = st.sidebar.text_input(label="Ad Account")
-st.sidebar.button(label="Submit")
-st.header("**This application let's you quickly import your ad data from facebook account 😎.**")
-if app_id != "" and app_secret != "" and access_token != "" and ad_account != "":
-    try:
-        credential = Logowanie(app_id, app_secret, access_token, ad_account)
-        dane = credential.login()
-        df = pd.DataFrame(data=dane)
-        st.markdown("Here you can find data frame with your data:")
-        st.write(df)
-        dane_csv = pd.DataFrame.to_csv(df)
-        st.success("Successfully connected to the database!")
-        st.download_button(label='Download your data', file_name='dane.csv', data=dane_csv)
-    except Exception as ex:
-        st.error(ex)
+option = st.sidebar.selectbox("Select what you want to do?",
+                              ('Read the data', 'Make the dashboard', 'Explore the data', 'Model your data'), 0)
 
+st.header(option)
 
+if option == 'Read the data':
+    st.sidebar.subheader("1. Enter your Facebook Developer API credentials:")
+    app_id = st.sidebar.text_input(label="App ID")
+    app_secret = st.sidebar.text_input(label="App Secret", type="password")
+    access_token = st.sidebar.text_input(label="Access Token")
+    ad_account = st.sidebar.text_input(label="Ad Account")
+    st.sidebar.subheader('2. Specify your data')
+    # Sidebar Options:
+    level = st.sidebar.selectbox('Level', ('ad', 'adset', 'campaign', 'account'))
+    date_preset = st.sidebar.selectbox('Period', ('last_7d', 'last_14d', 'last_28d', 'last_30d', 'last_90d'))
+    breakdowns = st.sidebar.selectbox('Breakdowns', ('region', 'gender', 'age', 'age, gender'))
+    st.header("**This application let's you quickly import your ad data from facebook account 😎.**")
+    st.sidebar.button(label="Submit")
+    if st.sidebar.button:
+        try:
+            credential = Logowanie(app_id, app_secret, access_token, ad_account, level, date_preset, breakdowns)
+            dane = credential.login()
+            df = pd.DataFrame(data=dane)
+            st.markdown("Here you can find data frame with your data:")
+            st.dataframe(df)
+            dane_csv = pd.DataFrame.to_csv(df)
+            st.success("Successfully connected to the database!")
+            st.download_button(label='Download your data', file_name='dane.csv', data=dane_csv)
+        except Exception as ex:
+            st.error(ex)
 
+if option == 'Make the dashboard':
+    st.sidebar.header("2. Choose your variables")
 
+if option == 'Explore the data':
+    st.sidebar.header("3. Make your exploratory analysis")
 
-
+if option == 'Model your data':
+    st.sidebar.header("4. Predict your ads performance")
--- data_pull.py
+++ data_pull.py
@@ -4,20 +4,23 @@ from facebook_business.adobjects.adaccount import AdAccount
 
 class Logowanie:
 
-    def __init__(self, app_id, app_secret, access_token, ad_account):
+    def __init__(self, app_id, app_secret, access_token, ad_account,
+                 level, date_preset, breakdowns):
         self.app_id = str(app_id)
         self.app_secret = str(app_secret)
         self.access_token = str(access_token)
         self.ad_account = str(ad_account)
+        self.level = str(level)
+        self.date_preset = str(date_preset)
+        self.breakdowns = str(breakdowns)
 
     def login(self):
         FacebookAdsApi.init(self.app_id, self.app_secret, self.access_token)
 
-        params = {'level': 'ad',
-                  'date_preset': 'last_30d',
-                  'sort': 'spend_descending',
+        params = {'level': self.level,
+                  'date_preset': self.date_preset,
                   'export_format': 'csv',
-                  'breakdowns': ['region']}
+                  'breakdowns': [self.breakdowns]}
 
         fields = ["account_id",
                   'ad_id',
